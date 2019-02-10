b7a719f551d5408ae88c52b82d70f961763a19ee
Robin Keith <robin.keith@yahoo.co.uk>
2019-02-10 10:17:28 +0000

    done

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
 
     }
 
-    showNextQuestionHandler(){
+    showNextQuestion(){
         this.setState((state) => {
             return {quiz_position:state.quiz_position+1}
         })
