4a69bdf88da1cfd50c8e7cd96bb7d1512bb54721
Robin Keith <robin.keith@yahoo.co.uk>
2019-02-10 10:38:36 +0000

    'done'

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -9,7 +9,7 @@ class QuizEnd extends Component {
         return (
             <div>
             <p>Thanks for playing!</p>
-            <a href='' obClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+            <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
             </div>
         )
     }
