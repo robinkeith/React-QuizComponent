be73b05ee4b3cbdaba87133c07686a6d3256baea
Robin Keith <robin.keith@yahoo.co.uk>
2019-02-10 10:12:54 +0000

    done

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component {
 
     }
 
+    showNextQuestionHandler(){
+        this.setState((state) => {
+            return {quiz_position:state.quiz_position+1}
+        })
+    }
+
     render(){
         const isQuizEnd=( (this.state.quiz_position -1)===quizData.quiz_questions.length)
         return (
@@ -18,7 +24,8 @@ class Quiz extends Component {
             <div>
             {isQuizEnd ?
                 <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                showNextQuestionHandler={this.showNextQuestionHandler.bind(this)}/>
             }
                 </div>
         )
