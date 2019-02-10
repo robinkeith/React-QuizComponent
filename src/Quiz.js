628e0cd6e32931cade27fa6927204024147ee41d
Robin Keith <robin.keith@yahoo.co.uk>
2019-02-10 10:39:58 +0000

    'done'

--- src/Quiz.js
+++ src/Quiz.js
@@ -26,7 +26,7 @@ class Quiz extends Component {
             
             <div>
             {isQuizEnd ?
-                <QuizEnd resetClickHandler={this.handleResetClick} /> :
+                <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                 showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
             }
